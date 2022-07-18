9fba4edf6f6e4030cae4947a3bddb39042beb87e
Stephen Womack <swomack111@gmail.com>
2022-07-18 09:11:21 -0500

    about

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,7 +9,17 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <h3>Technologies Used</h3>
+        <p>JavaScript, React, Redux, Sagas, Express, Node, Scryfall API</p>
+      </div>
+      <div>
+        <h3>What Next?</h3>
+        <p>Intelligent search result filtering.</p>
+        <p>Ability to share decks with other users within the application.</p>
+      </div>
+      <div>
+        <h3>Thanks To:</h3>
+        <p>Prime, Edan, the Gaiman Cohort, my parents.</p>
       </div>
     </div>
   );
